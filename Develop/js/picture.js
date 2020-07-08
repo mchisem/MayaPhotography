ac5a860451f5bdf101d6db25f06d8ffabf75a0f2
Maya Chisem <chisem.maya@gmail.com>
2020-07-07 21:40:02 -0700

    adding files

---
+++ Develop/js/picture.js
@@ -0,0 +1,28 @@
+// Variables
+var modal = document.getElementById("back");
+var span = document.querySelector("#close");
+var images = document.querySelector(".image");
+var modalImg = document.querySelector(".modal-content");
+var content = document.querySelector("#content");
+
+// Event delegation for images
+const parent = document.querySelector(".container");
+
+parent.addEventListener("click", event => {
+  if(event.target.className === "image") {
+  console.log(event.target)
+  modal.style.display = "block";
+
+  //if you console.log(this) -> returns the whole window object which has no src value
+  //using event.target can make us target exactly what we need
+  modalImg.src = event.target.src;
+  content.classList.remove("hide");
+  span.classList.remove("hide");
+  }
+  })
+
+// When the user clicks on (x), closes the modal
+span.onclick = function() {
+  modal.style.display = "none";
+  content.classList.add("hide");
+}
