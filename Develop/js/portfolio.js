dee28ef021eb430ad2d424211c12b1cf7cc2d8c0
Maya Chisem <chisem.maya@gmail.com>
2020-07-12 13:53:47 -0700

    update

--- Develop/js/portfolio.js
+++ Develop/js/portfolio.js
@@ -3,5 +3,6 @@ var loader = document.querySelector(".loader-wrapper");
 
 // while the page loads the loader icon will display
 $(window).on("load", function(){
+    // $("body").css({"overflow": "hidden"});
     $(loader).fadeOut("slow");
  });
